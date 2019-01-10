9fdd0f51c30b3229eda535f0284ff23280345330
SmartGanyaupfu <smartganyaupfu@gmail.com>
2019-01-10 21:10:48 +0200

    Jazzuary App

---
+++ JazzuaryFm/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JazzuaryFm
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
